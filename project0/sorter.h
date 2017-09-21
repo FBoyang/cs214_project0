2a0b57e715b0c60331bf79f6685efbf164852cff
Steve Hsu <triangular.pyramid@gmail.com>
2017-09-21 16:06:39 -0400

    added header file

---
+++ project0/sorter.h
@@ -0,0 +1,31 @@
+#ifndef _SORTER_H
+#define _SORTER_H
+struct csv {
+	int number_of_headers;
+	char **header_strings;
+	struct record_node *records;
+};
+struct record {
+	/*
+	 * the CSV file format specifies that each record
+	 * has the same number of entries, but I feel that
+	 * we should be cautious
+	 */
+	int length;
+	enum type_t *types;
+	void **values;
+};
+struct record_node {
+	struct record *data;
+	struct record_node *next;
+};
+enum type_t {
+	EMPTY;
+	NUMBER;
+	STRING;
+};
+
+struct csv *read_table();
+void print_table(const struct csv *table);
+void sort_by_field(struct csv *table, const char *field_name);
+#endif /* ifndef _SORTER_H */
